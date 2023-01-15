8f1c79430fd5adfc0acd68d101c4708583e7beef
Hisashi Iwamoto <hisasi622@gmail.com>
2023-01-15 13:17:36 +0900

    first commit

---
+++ src/routes/list.svelte
@@ -0,0 +1,13 @@
+<script>
+	let users = [
+		{ id: '1', name: 'Tanaka' },
+		{ id: '2', name: 'Suzuki' },
+		{ id: '3', name: 'Sato' }
+	];
+</script>
+
+<ul>
+	{#each users as user}
+		<li>{user.name}</li>
+	{/each}
+</ul>
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	let todos = [{ id: 1, title: 'テスト' }];
+	let todoTitle = '';
+	function getTodoLen() {
+		return todos.length + 1;
+	}
+
+	function add() {
+		todos = todos.concat({ id: getTodoLen(), title: todoTitle });
+		console.log(todos);
+	}
+</script>
+
+<svelte:head>
+	<title>Todo</title>
+	<meta name="description" content="Svelte todo app" />
+</svelte:head>
+
+<section>
+	<h1>Todo</h1>
+
+	<input type="text" bind:value={todoTitle} />
+	<button on:click={add}>追加</button>
+
+	<ul>
+		{#each todos as todo}
+			<li>
+				<input type="checkbox" />
+				<p>{todo.title}</p>
+				<p />
+			</li>
+		{/each}
+	</ul>
+</section>
+
+<style>
+	ul li {
+		display: flex;
+	}
+</style>
